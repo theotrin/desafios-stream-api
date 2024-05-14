ee566009b4d324c5f26a24496e9c46552ba9e40a
theotrin <theopinheiro12@gmail.com>
2024-05-14 09:41:22 -0400

    Desafio 10 refatorado

--- src/Desafio10.java
+++ src/Desafio10.java
@@ -8,7 +8,7 @@ public class Desafio10 {
     public static void main(String[] args) {
         List<Integer> numeros = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 5, 4, 3);
 
-        Predicate<Integer> numerosImpares = numero -> numero%2==0;
+        Predicate<Integer> numerosImpares = numero -> numero%2!=0;
         Predicate<Integer> numerosDivisiveis = numero -> numero%3==0 || numero%5==0 ;
 
         List<Integer> impares = numeros.stream()
